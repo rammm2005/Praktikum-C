1b563834ef18bffe0291be131aed1b54f5f55f2a
Rama Dita <ditarama985@gmail.com>
2023-10-15 07:03:17 -0700

    Fiks Variable naming for coding Art

--- c.c
+++ c.c
@@ -5,13 +5,15 @@ void groupName() {
     printf("\n");
     printf(" Group 15 programming practicum : \n");
     printf(" I Putu Rama Dita (2305551017) \n");
+    printf(" I Kadek Indriani Saputri (2305551062) \n");
+
     printf("\n");
 }
 
 void inputCheck(double *input) {
     while (scanf("%lf", input) != 1) {
         printf("Invalid input. Please input a valid input (Required Number): ");
-        while (getchar() != '\n'){
+        while (getchar() != '\n') {
             // The loop back code
         }
     }
@@ -19,7 +21,7 @@ void inputCheck(double *input) {
 
 int anyTriangle() {
 
-    double a, b, c, s, perimeter, area;
+    double side_a, side_b, side_c, semiperimeter, area, perimeter;
 
     printf(" ================ Calculating the Area and Perimeter of Any Triangle ================  \n");
     printf(" ====================================================================================  \n");
@@ -29,24 +31,24 @@ int anyTriangle() {
     printf("   /    \\  \n");
     printf("  /______\\  \n");
 
-    printf(" Enter the Value of a : ");
-    inputCheck(&a);
-    printf(" Enter the Value of b : ");
-    inputCheck(&b);
-    printf(" Enter the Value of c : ");
-    inputCheck(&c);
+    printf(" Enter the Value of side_a : ");
+    inputCheck(&side_a);
+    printf(" Enter the Value of side_b : ");
+    inputCheck(&side_b);
+    printf(" Enter the Value of side_c : ");
+    inputCheck(&side_c);
 
-    s = (a + b + c) / 2;
+    semiperimeter = (side_a + side_b + side_c) / 2;
 
-    area = sqrt(s * (s - a) * (s - b) * (s - c));
+    area = sqrt(semiperimeter * (semiperimeter - side_a) * (semiperimeter - side_b) * (semiperimeter - side_c));
 
-    perimeter = a + b + c;
+    perimeter = side_a + side_b + side_c;
 
     printf(" ====== Result ===== \n");
     printf(" area = A = √(s * (s - a) * (s - b) * (s - c) : \n");
     printf(" area = %.2lf\n\n", area);
     printf(" ====== Result ===== \n");
-    printf(" perimeter = a + b + c : \n");
+    printf(" perimeter = side_a + side_b + side_c : \n");
     printf(" perimeter = %.2lf\n", perimeter);
 
     return 0;
@@ -54,9 +56,9 @@ int anyTriangle() {
 
 int cutTheRiceCake() {
 
-    double side , perimeter , area , d1 ,d2;
+    double diagonal1, diagonal2, side_length, area, perimeter;
 
-    printf(" ================ Calculating the Area and Perimeter of cut The Rice Cake ================  \n");
+    printf(" ================ Calculating the Area and Perimeter of Cut The Rice Cake ================  \n");
     printf(" =========================================================================================  \n");
 
     printf("     //\\    \n");
@@ -68,32 +70,28 @@ int cutTheRiceCake() {
     printf("    \\  //  \n");
     printf("     \\//  \n");
 
+    printf(" Enter Diagonal 1 value : ");
+    inputCheck(&diagonal1);
+    printf(" Enter Diagonal 2 value : ");
+    inputCheck(&diagonal2);
+    printf(" Enter side length value : ");
+    inputCheck(&side_length);
 
-    printf(" Enter D1 value : ");
-    inputCheck(&d1);
-    printf(" Enter D2 value : ");
-    inputCheck(&d2);
-    printf(" Enter side value : ");
-    inputCheck(&side);
-
-    area = 0.5 * (d1 * d2);
-    perimeter = side + side + side + side;
+    area = 0.5 * (diagonal1 * diagonal2);
+    perimeter = 4 * side_length;
 
     printf(" ====== Result ===== \n");
-    printf(" area = 1/2 * (d1 * d2) : \n");
+    printf(" area = 1/2 * (diagonal1 * diagonal2) : \n");
     printf(" area = %.2lf\n\n", area);
     printf(" ====== Result ===== \n");
-    printf(" perimeter = 4*(side): \n");
+    printf(" perimeter = 4 * (side_length): \n");
     printf(" perimeter = %.2lf\n", perimeter);
 
     return 0;
-
 }
 
-
-
-int paralleLogram(){
-    double base , height , area , perimeter;
+int paralleLogram() {
+    double base, height, area, perimeter;
 
     printf(" ================ Calculating the Area and Perimeter of Parallelogram ===================  \n");
     printf(" =========================================================================================  \n");
@@ -110,20 +108,19 @@ int paralleLogram(){
 
     area = base * height;
     perimeter = 2 * (base + height);
-    
 
     printf(" ====== Result ===== \n");
     printf(" area = base * height : \n");
     printf(" area = %.2lf\n\n", area);
     printf(" ====== Result ===== \n");
-    printf(" perimeter = 2 * (base * height) : \n");
+    printf(" perimeter = 2 * (base + height) : \n");
     printf(" perimeter = %.2lf\n", perimeter);
 
     return 0;
 }
 
-int trapezium(){
-    double a , b , c , d , h, area , perimeter;
+int trapezium() {
+    double longer_base_a, longer_base_b, shorter_base_c, shorter_base_d, height, area, perimeter;
 
     printf(" ================== Calculating the Area and Perimeter of Trapezium ======================  \n");
     printf(" =========================================================================================  \n");
@@ -134,20 +131,18 @@ int trapezium(){
     printf(" *****************************\n");
 
     printf(" Enter the length of the longer base (a): ");
-    inputCheck(&a);
+    inputCheck(&longer_base_a);
     printf(" Enter the length of the longer base (b): ");
-    inputCheck(&b);
+    inputCheck(&longer_base_b);
     printf(" Enter the length of not the longer base (c): ");
-    inputCheck(&c);
+    inputCheck(&shorter_base_c);
     printf(" Enter the length of the other not longer base (d): ");
-    inputCheck(&d);
+    inputCheck(&shorter_base_d);
     printf(" Enter the height base (h): ");
-    inputCheck(&h);
+    inputCheck(&height);
 
-
-    area = 0.5 * (a + b) * h;
-    perimeter = a + b + c + d ;
-    
+    area = 0.5 * (longer_base_a + longer_base_b) * height;
+    perimeter = longer_base_a + longer_base_b + shorter_base_c + shorter_base_d;
 
     printf(" ====== Result ===== \n");
     printf(" area = 1/2 * (a + b) * h : \n");
@@ -159,36 +154,30 @@ int trapezium(){
     return 0;
 }
 
-
-
-int circle(){
-    double r , area , perimeter;
+int circle() {
+    double radius, area, circumference;
 
     printf(" ==================== Calculating the Area and Perimeter of Circle =======================  \n");
     printf(" =========================================================================================  \n");
-   
 
     printf(" Enter the radius of Circle : ");
-    inputCheck(&r);
+    inputCheck(&radius);
 
-
-    area = 3.14159 * r * r;
-    perimeter = 2 * 3.14159 * r;
-    
+    area = 3.14159 * radius * radius;
+    circumference = 2 * 3.14159 * radius;
 
     printf(" ====== Result ===== \n");
-    printf(" area = π * r^2) : \n");
+    printf(" area = π * r^2 : \n");
     printf(" area = %.2lf\n\n", area);
     printf(" ====== Result ===== \n");
-    printf(" perimeter = 2 * π * r : \n");
-    printf(" perimeter = %.2lf\n", perimeter);
+    printf(" circumference = 2 * π * r : \n");
+    printf(" circumference = %.2lf\n", circumference);
 
     return 0;
 }
 
 int main() {
-    char choose;
-
+    char choice;
 
     groupName();
     printf(" Program to calculate the area and perimeter of flat shapes:\n");
@@ -199,15 +188,15 @@ int main() {
     printf(" 4. Trapezium\n");
     printf(" 5. Circle\n");
 
-    do{
-        int invalidChoose = 0;
+    do {
+        int invalidChoice = 0;
         printf(" Enter your choice (1-5) or q to exit: ");
-        scanf(" %c" , &choose);
+        scanf(" %c", &choice);
 
-         while (getchar() != '\n'){
-                
+        while (getchar() != '\n') {
+            // Clear the input buffer
         }
-        switch (choose){
+        switch (choice) {
         case '1':
             anyTriangle();
             break;
@@ -226,18 +215,16 @@ int main() {
 
         case '5':
             circle();
-        break;
+            break;
 
         case 'q':
             return 0;
-        break;
 
         default:
-          printf(" Input invalid. Please input the true statement (1-5 or q to exit). \n ");
-          int invalidChoose = 1;
+            printf(" Input invalid. Please input the correct option (1-5 or q to exit). \n ");
+            invalidChoice = 1;
         }
-
-    } while(choose != 'q');
+    } while (choice != 'q');
 
     return 0;
 }
